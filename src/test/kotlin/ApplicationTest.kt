84430ab521fdcbc549000c7a537c8962bf6bdb70
ndwPC <magma0409@gmail.com>
2025-11-21 20:17:00 +0900

    test: 구매한 개수만큼 로또 발행하기

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -8,39 +8,48 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class ApplicationTest : NsTest() {
+    private val BUY: String = "4000"
+    private val COUNT: String = "4개를 구매했습니다."
     private val LOTTO: String = "1,2,3,4,5,6"
     private val BONUS: String = "7"
 
     @Test
     fun 기능_당첨번호_입력() {
         assertSimpleTest {
-            run("8000", "1", LOTTO, BONUS)
-            assertThat(output()).contains("8개를 구매했습니다.", "[1, 2, 3, 4, 5, 6]")
+            run(BUY, "1", LOTTO, BONUS)
+            assertThat(output()).contains(COUNT)
         }
     }
 
     @Test
     fun 기능_당첨번호_미입력() {
+        assertSimpleTest {
+            run(BUY, "2")
+            assertThat(output()).contains(COUNT)
+        }
+    }
+
+    @Test
+    fun 기능_당첨번호_미입력2() {
         assertRandomUniqueNumbersInRangeTest(
             {
-                run("8000", "2")
+                run(BUY, "2")
                 assertThat(output()).contains(
-                    "8개를 구매했습니다.",
+                    COUNT,
                     "[8, 21, 23, 41, 42, 43]"
                 )
             },
-            mutableListOf<Int>(41, 42, 8, 21, 23, 43, 3)
+            mutableListOf<Int>(41, 42, 8, 21, 23, 43)
         )
     }
 
     @Test
     fun 예외_구입금액에_문자_입력() {
         assertSimpleTest {
-            run("a", "8000", "1", LOTTO, BONUS)
+            run("a", BUY, "1", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 구입금액에 숫자만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -48,11 +57,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_구입금액에_0_입력() {
         assertSimpleTest {
-            run("0", "8000", "1", LOTTO, BONUS)
+            run("0", BUY, "1", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 로또를 사셔야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -60,11 +68,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_구입금액에_100단위_입력() {
         assertSimpleTest {
-            run("800", "8000", "1", LOTTO, BONUS)
+            run("800", BUY, "1", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 구입 금액은 1,000원 단위여야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -72,11 +79,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_선택번호_잘못_입력() {
         assertSimpleTest {
-            run("8000", "3", "1", LOTTO, BONUS)
+            run(BUY, "3", "1", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 1 또는 2만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -84,11 +90,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_당첨번호_문자_입력() {
         assertSimpleTest {
-            run("8000", "1", "1,a,3,4,5,6", LOTTO, BONUS)
+            run(BUY, "1", "1,a,3,4,5,6", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 숫자만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -96,11 +101,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_당첨번호_범위밖_입력() {
         assertSimpleTest {
-            run("8000", "1", "1,0,3,4,5,6", LOTTO, BONUS)
+            run(BUY, "1", "1,0,3,4,5,6", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 1 ~ 45의 숫자만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -108,11 +112,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_당첨번호_중복_입력() {
         assertSimpleTest {
-            run("8000", "1", "1,1,3,4,5,6", LOTTO, BONUS)
+            run(BUY, "1", "1,1,3,4,5,6", LOTTO, BONUS)
             assertThat(output()).contains(
                 "[ERROR] 서로 다른 숫자만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -120,11 +123,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_보너스번호_잘못된_값_입력() {
         assertSimpleTest {
-            run("8000", "1", "1,1,3,4,5,6", LOTTO, "a", "7")
+            run(BUY, "1", "1,1,3,4,5,6", LOTTO, "a", "7")
             assertThat(output()).contains(
                 "[ERROR] 숫자만 입력해야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
@@ -132,11 +134,10 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 예외_보너스번호_당첨번호와_같은_값_입력() {
         assertSimpleTest {
-            run("8000", "1", "1,1,3,4,5,6", LOTTO, "4", "7")
+            run(BUY, "1", "1,1,3,4,5,6", LOTTO, "4", "7")
             assertThat(output()).contains(
                 "[ERROR] 보너스 번호는 당첨번호들과 달라야 합니다.",
-                "8개를 구매했습니다.",
-                "[1, 2, 3, 4, 5, 6]"
+                COUNT
             )
         }
     }
