80dadf84221ae7224accf29e2a826157869f9e86
ndwPC <magma0409@gmail.com>
2025-11-21 20:41:56 +0900

    test: 발행된 로또 별 당첨여부 확인하기

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -5,18 +5,40 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class LottoTest {
+    private val LOTTO: Lotto = Lotto(listOf(4, 5, 6, 1, 2, 3))
 
     @Test
     fun 기능_번호출력() {
         assertSimpleTest {
-            Lotto(listOf(5, 6, 3, 4, 2, 1)).print()
+            LOTTO.print()
         }
     }
 
     @Test
     fun 기능_보너스번호_포함여부() {
         assertSimpleTest {
-            println(Lotto(listOf(1, 2, 3, 4, 5, 6)).checkBonus(4))
+            println(LOTTO.check(4))
+        }
+    }
+
+    @Test
+    fun 기능_1등() {
+        assertSimpleTest {
+            println(LOTTO.match(LOTTO, 7))
+        }
+    }
+
+    @Test
+    fun 기능_2등() {
+        assertSimpleTest {
+            println(LOTTO.match(Lotto(listOf(1, 2, 3, 4, 5, 7)), 6))
+        }
+    }
+
+    @Test
+    fun 기능_3등() {
+        assertSimpleTest {
+            println(LOTTO.match(Lotto(listOf(1, 2, 3, 4, 5, 7)), 8))
         }
     }
 
