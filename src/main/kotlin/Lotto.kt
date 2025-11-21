b88c347c795446ba63ef0afdb139b749ef702bbc
ndwPC <magma0409@gmail.com>
2025-11-21 16:07:15 +0900

    feat: 로또 클래스 만들기

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -2,6 +2,16 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
+        require(numbers.all { it in 1..45 }) {
+            "[ERROR] 1 ~ 45의 숫자만 입력해야 합니다."
+        }
+
+        require(numbers.distinct().size == numbers.size) {
+            "[ERROR] 서로 다른 숫자만 입력해야 합니다."
+        }
+    }
+
+    fun match(win: Lotto, bonus: Int) {
 
     }
 }
