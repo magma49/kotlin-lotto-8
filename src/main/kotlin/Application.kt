3f4b502af379750d9982f481719c1730bb61353c
ndwPC <magma0409@gmail.com>
2025-11-21 14:51:08 +0900

    style: 구현한 기능 함수화

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -15,7 +15,6 @@ fun main() {
 fun getValidPurchase(): Int {
     while (true) {
         try {
-            println("구입금액을 입력해 주세요.")
             return validPurchase()
         } catch (e: IllegalArgumentException) {
             println(e.message)
@@ -26,6 +25,7 @@ fun getValidPurchase(): Int {
 fun validPurchase(): Int {
     val purchase: Int
     try {
+        println("구입금액을 입력해 주세요.")
         purchase = Console.readLine().toInt()
     } catch (e: NumberFormatException) {
         throw java.lang.IllegalArgumentException("[ERROR] 구입금액에 숫자만 입력해야 합니다.")
@@ -40,23 +40,25 @@ fun validPurchase(): Int {
 fun choose(): List<Int> {
     while (true) {
         try {
-            println("당첨번호를 입력하시겠습니까? 입력을 하시려면 1, 입력을 안 하시려면 2를 입력해 주세요.")
-            if (validChoose() == 1) {
-                val win: List<Int> = getValidWin()
-                val bonus: Int = getValidBonus()
-                return win + bonus
-            } else {
-                return getRandomWin()
-            }
+            return getValidChoose()
         } catch (e: IllegalArgumentException) {
             println(e.message)
         }
     }
 }
 
+fun getValidChoose(): List<Int> {
+    if (validChoose() == 1) {
+        return getValidWin() + getValidBonus()
+    } else {
+        return getRandomWin()
+    }
+}
+
 fun validChoose(): Int {
     val choice: Int
     try {
+        println("당첨번호를 입력하시겠습니까? 입력을 하시려면 1, 입력을 안 하시려면 2를 입력해 주세요.")
         choice = Console.readLine().toInt()
     } catch (e: NumberFormatException) {
         throw java.lang.IllegalArgumentException("[ERROR] 1 또는 2만 입력해야 합니다.")
