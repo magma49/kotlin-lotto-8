c26372707495b79818b42a9560fc0a382793be95
ndwPC <magma0409@gmail.com>
2025-11-21 16:07:28 +0900

    test: 로또 클래스 만들기

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -16,8 +16,16 @@ class LottoTest {
     }
 
     @Test
-    fun 예외() {
-        assertThatThrownBy { Lotto(listOf()) }
+    fun 예외_당첨번호_범위밖_입력() {
+        assertThatThrownBy { Lotto(listOf(1, 0, 3, 4, 5, 6)) }
             .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR] 1 ~ 45의 숫자만 입력해야 합니다.")
+    }
+
+    @Test
+    fun 예외_당첨번호_중복_입력() {
+        assertThatThrownBy { Lotto(listOf(1, 1, 3, 4, 5, 6)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR] 서로 다른 숫자만 입력해야 합니다.")
     }
 }
